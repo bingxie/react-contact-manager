3fa8a3105fc2649e50da12ed35c9a9a1dcf540e6
Bing Xie <bing708@gmail.com>
2019-04-08 16:51:21 +1000

    New contact manager app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Contact from "./components/Contact";
+import Header from "./components/Header";
+
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Header branding="Concact Manager" />
+        <Contact name="John Doe" email="email@example.com" phone="3243242" />
+        <Contact
+          name="Karen Smith"
+          email="karen@gmail.com"
+          phone="333-333-3333"
+        />
       </div>
     );
   }
