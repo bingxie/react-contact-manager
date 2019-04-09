b7df1ab9166926b31333c3e3210c2d87a4796a52
Bing Xie <bing708@gmail.com>
2019-04-09 10:57:15 +1000

    Add bootstrap to style the page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Contact from "./components/Contact";
 import Header from "./components/Header";
 
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 class App extends Component {
@@ -9,12 +10,14 @@ class App extends Component {
     return (
       <div className="App">
         <Header branding="Concact Manager" />
-        <Contact name="John Doe" email="email@example.com" phone="3243242" />
-        <Contact
-          name="Karen Smith"
-          email="karen@gmail.com"
-          phone="333-333-3333"
-        />
+        <div className="container">
+          <Contact name="John Doe" email="email@example.com" phone="3243242" />
+          <Contact
+            name="Karen Smith"
+            email="karen@gmail.com"
+            phone="333-333-3333"
+          />
+        </div>
       </div>
     );
   }
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,9 +5,20 @@ const Header = props => {
   const { branding } = props;
 
   return (
-    <div>
-      <h1>{branding}</h1>
-    </div>
+    <nav className="navbar navbar-expand-sm navbar-dark bg-danger mb-3 py-0">
+      <div className="container">
+        <a href="/" className="navbar-brand">
+          {branding}
+        </a>
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item">
+            <a href="/" className="nav-link">
+              Home
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 };
 
